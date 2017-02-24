f9d5c95fe1bd2a4ed57dc043a3b216cfcf23148e
Lorna Sherwood <lornasherwood@gmail.com>
2017-02-24 11:56:27 +0000

    add game.dealCards

--- client/src/models/game.js
+++ client/src/models/game.js
@@ -8,9 +8,17 @@ var Game = function(){
 
 Game.prototype = {
 
-
- 
+  dealCards: function(Deck){
+    for (var i = 0; i < Deck.deck.length/2; i++){
+      this.playerHand.push(Deck.deck[i])
+    };
+    for (var i = Deck.deck.length/2; i < Deck.deck.length; i++){
+      this.computerHand.push(Deck.deck[i])
+    };
+  }
 }
+
+
 
 
 module.exports = Game;
--- client/src/models/specs/game-spec.js
+++ client/src/models/specs/game-spec.js
@@ -1,12 +1,17 @@
 var Game = require('../game');
+var Deck = require('../deck');
 
 var assert = require('assert');
 
 describe('Game', function() {
   var game1;
+  var deckA;
 
   beforeEach(function() {
     game1 = new Game();
+    deckA = new Deck();
+    cards = [{name: 1}, {name: 1}, {name: 1}, {name: 1}, {name: 1}, {name: 1}]
+
   });
 
   it('should start with empty playerHand', function() {
@@ -15,6 +20,18 @@ describe('Game', function() {
 
   it('should start with empty computerHand', function() {
     assert.equal(0, game1.computerHand.length)
+  });
+
+  it('should add cards to computerHand', function() {
+    deckA.getCards(cards)
+    game1.dealCards(deckA)
+    assert.equal(3, game1.computerHand.length)
+  });
+
+  it('should add cards to playerHand', function() {
+    deckA.getCards(cards)
+    game1.dealCards(deckA)
+    assert.equal(3, game1.playerHand.length)
   });
 
 })
