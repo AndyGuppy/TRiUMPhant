40281f6aa228ba99b299cf685e85551aea70f5bb
andrew guppy <andrew.guppy@blueyonder.co.uk>
2017-02-24 10:36:08 +0000

    seed file seeded'

--- seeds.js
+++ seeds.js
@@ -2,11 +2,48 @@ use trumps_cities;
 
 db.cities.insert([
   { 
-    
+      name: "Shanghai",
+      imagepth: "/shanghai.jpg",
+      skycode:"SHA",
+      country: "China",
+      flag: "china.gif"
   },
   { 
-    
-  }]
+      name: "Dehli",
+      imagepth: "/dehli.jpg",
+      skycode:"DEL",
+      country: "India",
+      flag: "india.gif"
+  },
+  { 
+      name: "Istanbul",
+      imagepth: "/istanbul.jpg",
+      skycode:"IST",
+      country: "Turkey",
+      flag: "turkey.gif"
+  },
+    { 
+      name: "Tokyo",
+      imagepth: "/tokyo.jpg",
+      skycode:"HND",
+      country: "Japan",
+      flag: "japan.gif"
+    },
+    { 
+      name: "Moscow",
+      imagepth: "/moscow.jpg",
+      skycode:"DME",
+      country: "Russia",
+      flag: "russia.gif"
+    },
+    { 
+      name: "New York",
+      imagepth: "/newyork.jpg",
+      skycode:"JFK",
+      country: "United States",
+      flag: "unitedstates.gif"
+    }
+  ]
 );
 
 // db.cities.find();
